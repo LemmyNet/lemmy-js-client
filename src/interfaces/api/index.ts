e812a627b153262aeff206dd45ef3aedacbb84c6
Dessalines <tyhou13@gmx.com>
2020-12-20 13:50:08 -0500

    Adding index.ts s

---
+++ src/interfaces/api/index.ts
@@ -0,0 +1,5 @@
+export * from './comment';
+export * from './community';
+export * from './post';
+export * from './site';
+export * from './user';
---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export * from './api';
+export * from './aggregates';
+export * from './others';
+export * from './source';
+export * from './views';
