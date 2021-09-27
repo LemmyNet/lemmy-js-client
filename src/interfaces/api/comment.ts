a5d94056db34ffc7f007996daf2fcc0091bc1288
Dessalines <tyhou13@gmx.com>
2021-09-27 14:48:27 -0400

    Adding unresolved_only

--- src/interfaces/api/comment.ts
+++ src/interfaces/api/comment.ts
@@ -121,6 +121,11 @@ export interface ListCommentReports {
    * if no community is given, it returns reports for all communities moderated by the auth user.
    */
   community_id?: number;
+
+  /**
+   * Only shows the unresolved reports.
+   */
+  unresolved_only?: boolean;
   auth: string;
 }
 
--- src/interfaces/api/post.ts
+++ src/interfaces/api/post.ts
@@ -143,7 +143,11 @@ export interface ListPostReports {
   /**
    * if no community is given, it returns reports for all communities moderated by the auth user.
    */
-  community?: number;
+  community_id?: number;
+  /**
+   * Only shows the unresolved reports.
+   */
+  unresolved_only?: boolean;
   auth: string;
 }
 
