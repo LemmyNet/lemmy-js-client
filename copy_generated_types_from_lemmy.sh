40dfb4c3ac33621be1df91b9ba614c5d2f35aa37
SleeplessOne1917 <abias1122@gmail.com>
2023-05-11 17:10:28 +0000

    Make script for adding types to index (#118)
    * Make script for adding types to index
    
    * Add semicolons to export lines

--- copy_generated_types_from_lemmy.sh
+++ copy_generated_types_from_lemmy.sh
@@ -12,3 +12,5 @@ cp db_schema/bindings/* ../../lemmy-js-client/src/types/
 cp db_views/bindings/* ../../lemmy-js-client/src/types/
 cp db_views_actor/bindings/* ../../lemmy-js-client/src/types/
 cp db_views_moderator/bindings/* ../../lemmy-js-client/src/types/
+
+node ../../lemmy-js-client/putTypesInIndex.js
---
+++ putTypesInIndex.js
@@ -0,0 +1,36 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const exportRegex = /export\s+(?:enum|interface|type)\s+([A-Za-z0-9_]+)/g;
+
+const baseExports = ['export * from "./http";', 'export * from "./websocket";'];
+
+async function putTypesInIndex() {
+  const typeFiles = await fs.readdir(path.resolve("./src/types"));
+  const exports = [...baseExports];
+  for (const filename of typeFiles) {
+    const localExports = [];
+    const fileText = await fs.readFile(path.resolve(`./src/types/${filename}`));
+    for (
+      let match = exportRegex.exec(fileText);
+      match;
+      match = exportRegex.exec(fileText)
+    ) {
+      localExports.push(match[1]);
+    }
+
+    const spacer = localExports.length > 1 ? "\n" : " ";
+
+    exports.push(
+      `export {${spacer}${localExports.join(
+        ",\n"
+      )}${spacer}} from "./types/${filename.replace(/\..+/, "")}";`
+    );
+  }
+
+  fs.writeFile(path.resolve("./src/index.ts"), exports.join("\n"), {
+    flag: "w",
+  });
+}
+
+putTypesInIndex();
