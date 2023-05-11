5a44498b6181110d8af8682924ae3d89ece2e05d
SleeplessOne1917 <abias1122@gmail.com>
2023-05-11 08:19:50 -0400

    Add semicolons to export lines

--- putTypesInIndex.js
+++ putTypesInIndex.js
@@ -24,7 +24,7 @@ async function putTypesInIndex() {
     exports.push(
       `export {${spacer}${localExports.join(
         ",\n"
-      )}${spacer}} from "./types/${filename.replace(/\..+/, "")}"`
+      )}${spacer}} from "./types/${filename.replace(/\..+/, "")}";`
     );
   }
 
