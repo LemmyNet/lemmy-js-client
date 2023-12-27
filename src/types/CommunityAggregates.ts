d2d6f54c59f6d7c7dc736758fafe0a72acdcca94
İsmail Karslı <cszn@pm.me>
2023-12-27 18:03:23 +0300

    Rename local_subscribers to subscribers_local

--- src/types/CommunityAggregates.ts
+++ src/types/CommunityAggregates.ts
@@ -11,5 +11,5 @@ export interface CommunityAggregates {
   users_active_week: /* integer */ number;
   users_active_month: /* integer */ number;
   users_active_half_year: /* integer */ number;
-  local_subscribers: /* integer */ number;
+  subscribers_local: /* integer */ number;
 }
