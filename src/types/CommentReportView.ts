57c6df2aaef5d9d4664347eca8925694b8ada52d
Dessalines <tyhou13@gmx.com>
2024-03-13 13:41:27 -0400

    Adding vote display modes and other additions from main.

--- src/types/CommentReportView.ts
+++ src/types/CommentReportView.ts
@@ -5,6 +5,7 @@ import type { CommentReport } from "./CommentReport";
 import type { Community } from "./Community";
 import type { Person } from "./Person";
 import type { Post } from "./Post";
+import type { SubscribedType } from "./SubscribedType";
 
 export interface CommentReportView {
   comment_report: CommentReport;
@@ -15,6 +16,11 @@ export interface CommentReportView {
   comment_creator: Person;
   counts: CommentAggregates;
   creator_banned_from_community: boolean;
+  creator_is_moderator: boolean;
+  creator_is_admin: boolean;
+  creator_blocked: boolean;
+  subscribed: SubscribedType;
+  saved: boolean;
   my_vote?: number;
   resolver?: Person;
 }
