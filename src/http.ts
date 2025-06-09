3d564d962d9901fa0b9ad96246f553352aefce90
Dessalines <dessalines@users.noreply.github.com>
2025-06-09 17:12:43 -0400

    Use the error name and message for errors. (#599)
    * Use the error name and message for errors.
    
    * 1.0.0-name-and-message.1

--- src/http.ts
+++ src/http.ts
@@ -2790,7 +2790,12 @@ export class LemmyHttp extends Controller {
     }
 
     if (!response.ok) {
-      throw new Error(json["error"] ?? response.statusText);
+      let err: Error = {
+        name: json.error ?? response.statusText,
+        // Leave an empty error message if undefined
+        message: json.message ?? "",
+      };
+      throw err;
     } else {
       return json;
     }
