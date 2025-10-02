96ba5ab3854114ed57e342ffaf5eec2fa5862b60
Nutomic <me@nutomic.com>
2025-10-02 19:37:56 +0000

    Create array with all errors (#720)
    * Create array with all errors
    
    * 1.0.0-errors-array.0
    
    * fix
    
    * 1.0.0-errors-array.2
    
    * add type
    
    * 1.0.0-errors-array.3
    
    ---------
    
    Co-authored-by: Dessalines <dessalines@users.noreply.github.com>

--- scripts/copy_generated_types_from_lemmy.sh
+++ scripts/copy_generated_types_from_lemmy.sh
@@ -42,6 +42,14 @@ find src/types -type f -name '*.ts' -exec sed -i 's/bigint/number/g' {} +
 # on MacOS:
 # find src/types -type f -name '*.ts' -exec sed -i '' -e 's/bigint/number/g' {} \;
 
+# Parse LemmyErrorType and convert it to array.
+ALL_ERRORS=$(cat src/types/LemmyErrorType.ts | \
+perl -nle 'm/\{ \s*error:\s*"(.*?)" \}/; print $1' | \
+sed '/^[[:space:]]*$/d'| \
+sed 's/^/"/;s/$/"/' | \
+tr '\n' ',')
+echo "export let AllLemmyErrors = [$ALL_ERRORS];" > src/types/AllLemmyErrors.ts
+
 node putTypesInIndex.js
 
 pnpm prettier -w src
