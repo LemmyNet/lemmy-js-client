06cffe15991e488756314fa75f883ca16a121d12
Dessalines <dessalines@users.noreply.github.com>
2025-10-08 16:08:26 +0800

    Updating API types (#721)
    * Updating API types
    
    * More updates.

--- scripts/copy_generated_types_from_lemmy.sh
+++ scripts/copy_generated_types_from_lemmy.sh
@@ -44,12 +44,8 @@ find src/types -type f -name '*.ts' -exec sed -i 's/bigint/number/g' {} +
 
 # Parse LemmyErrorType and convert it to array.
 pnpm prettier -w src/types/LemmyErrorType.ts
-ALL_ERRORS=$(cat src/types/LemmyErrorType.ts | \
-perl -nle 'm/\{ \s*error:\s*"(.*?)" \}/; print $1' | \
-sed '/^[[:space:]]*$/d'| \
-sed 's/^/"/;s/$/"/' | \
-tr '\n' ',')
-echo "export let AllLemmyErrors = [$ALL_ERRORS];" > src/types/AllLemmyErrors.ts
+ALL_ERRORS=$(cat src/types/LemmyErrorType.ts | perl -nle 'm/\{ \s*error:\s*"(.*?)" \}/; print $1' | sed '/^[[:space:]]*$/d' | sed 's/^/"/;s/$/"/' | tr '\n' ',')
+echo "export const AllLemmyErrors = [$ALL_ERRORS];" >src/types/AllLemmyErrors.ts
 
 node putTypesInIndex.js
 
