720ff0e04deb83feea494f6094fc0507e3b090c1
Felix Ableitner <me@nutomic.com>
2025-12-03 11:19:03 +0100

    Rename PagedResponse.data to items

--- src/other_types.ts
+++ src/other_types.ts
@@ -47,7 +47,8 @@ export interface SearchI extends Search {}
 export interface ResolveObjectI extends ResolveObject {}
 export interface GetCommunityI extends GetCommunity {}
 export interface ListCommunitiesI extends ListCommunities {}
-export interface ListCommunityPendingFollowsI extends ListCommunityPendingFollows {}
+export interface ListCommunityPendingFollowsI
+  extends ListCommunityPendingFollows {}
 export interface GetRandomCommunityI extends GetRandomCommunity {}
 export interface GetPostI extends GetPost {}
 export interface GetPostsI extends GetPosts {}
@@ -64,8 +65,10 @@ export interface ListPersonSavedI extends ListPersonSaved {}
 export interface ListPersonReadI extends ListPersonRead {}
 export interface ListPersonHiddenI extends ListPersonHidden {}
 export interface ListPersonLikedI extends ListPersonLiked {}
-export interface ListRegistrationApplicationsI extends ListRegistrationApplications {}
-export interface GetRegistrationApplicationI extends GetRegistrationApplication {}
+export interface ListRegistrationApplicationsI
+  extends ListRegistrationApplications {}
+export interface GetRegistrationApplicationI
+  extends GetRegistrationApplication {}
 export interface ListCustomEmojisI extends ListCustomEmojis {}
 export interface ListTaglinesI extends ListTaglines {}
 export interface ListReportsI extends ListReports {}
