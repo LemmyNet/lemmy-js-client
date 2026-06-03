8e198a675dd5d9d5726a223a091ab577c5d1ba85
Nutomic <me@nutomic.com>
2026-06-03 15:20:12 +0200

    API docs without RequestState (#977)
    * Try generic typing of wrapper output for useRequestState init option.
    
    * Add useRequestState to README.
    
    * API docs without RequestState
    
    * git
    
    * cleanup
    
    ---------
    
    Co-authored-by: Dessalines <tyhou13@gmx.com>

--- src/request_state.ts
+++ src/request_state.ts
@@ -18,16 +18,3 @@ export type RequestState<T> =
   | LoadingRequestState
   | FailedRequestState
   | SuccessRequestState<T>;
-
-export function mapToRequestState<ResultT>(
-  result?: ResultT,
-  error?: Error,
-): RequestState<ResultT> {
-  if (error) {
-    return { state: "failed", err: error };
-  }
-  if (result !== undefined) {
-    return { state: "success", data: result };
-  }
-  return { state: "empty" };
-}
