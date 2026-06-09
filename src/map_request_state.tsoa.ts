790e5f265c43450093d1d0d35ab7f93285e6911a
Dessalines <dessalines@users.noreply.github.com>
2026-06-09 10:04:10 -0400

    Fixing tsoa build (#986)

--- src/map_request_state.tsoa.ts
+++ src/map_request_state.tsoa.ts
@@ -1,6 +1,5 @@
 // Does not return `RequestState<ResultT>` so that it doesnt become part of OpenAPI spec.
 export function mapToRequestState<ResultT>(
-  _useRequestState: boolean,
   result?: ResultT,
   _error?: Error,
 ): ResultT | undefined {
